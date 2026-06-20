db8879e960770db6aec4704fb02d53ca770e2f87
Prabhjot <sainiprabhjot75@gmail.com>
2026-06-20 18:27:13 +0530

    Changing the CORS to live link!

--- retro_backend/index.js
+++ retro_backend/index.js
@@ -8,12 +8,12 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "config.env") });
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://retro-sync-olive.vercel.app/" }));
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://retro-sync-olive.vercel.app/",
   },
 });
 
