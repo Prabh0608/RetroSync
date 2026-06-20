f438b2a189e47a6aeb6e234a2c00621ce76c1480
Prabhjot <sainiprabhjot75@gmail.com>
2026-06-20 18:09:51 +0530

    getting ready for deployment

--- retro_backend/index.js
+++ retro_backend/index.js
@@ -95,6 +95,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen("3000", () => {
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
   console.log("Your server is running on port 3000!");
 });
